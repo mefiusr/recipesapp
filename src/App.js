c7d7c1ed29469e3ac3bcf789dcfb235584ec54b1
Mathews <mathewsrodrigues9@gmail.com>
2022-09-05 11:50:38 -0300

    ajustando arquivo app e index

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Drinks from './pages/Drinks';
+import Foods from './pages/Foods';
+import Profile from './components/Profile';
+import FavoriteRecipes from './components/FavoriteRecipes';
+import DoneRecipes from './pages/DoneRecipes';
+import Food from './pages/Food';
+import Drink from './pages/Drink';
+import FoodInProgrees from './pages/FoodInProgress';
+import DrinkInProgrees from './pages/DrinkInProgress';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SHOW
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+      <Route exact path="/foods" render={ (props) => <Foods { ...props } /> } />
+      <Route exact path="/drinks" component={ Drinks } />
+      <Route exact path="/foods/:id" component={ Food } />
+      <Route exact path="/drinks/:id" component={ Drink } />
+      <Route
+        exact
+        path="/foods/:id/in-progress"
+        render={ (props) => <FoodInProgrees { ...props } /> }
+      />
+      <Route
+        path="/drinks/:id/in-progress"
+        render={ (props) => <DrinkInProgrees { ...props } /> }
+      />
+      <Route path="/profile" component={ Profile } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+      <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+    </Switch>
   );
 }
 
